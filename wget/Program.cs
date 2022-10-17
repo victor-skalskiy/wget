c40d18be5bb341c3848d9eaeff0854e1d04a0798
dasp <victor@skalskiy.net>
2022-10-17 22:47:06 +0300

    main one time commit

---
+++ wget/Program.cs
@@ -0,0 +1,8 @@
+﻿var pars = Environment.GetCommandLineArgs();
+if (pars.Length <= 1)
+    return;
+
+var result = await (new HttpClient()).GetAsync(pars[1]);
+var responseText = await result.Content.ReadAsStringAsync();
+
+Console.WriteLine(responseText);
